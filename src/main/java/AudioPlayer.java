b759f3650dcd82ed9e17fd96ccaf03be99e532d7
Ronald Q <rjquiroz0802@eagle.fgcu.edu>
2020-10-02 15:01:50 -0400

    activity week 7: issue 2 just implemented a new interface, and a new class.

---
+++ src/main/java/AudioPlayer.java
@@ -0,0 +1,37 @@
+public class AudioPlayer extends Product implements MultimediaControl {
+
+    String supportedAudioFormats;
+    String SupportedPlaylistFormats;
+
+    public AudioPlayer(String name, String manufacturer, String supportedAudioFormats, String SupportedPlaylistFormats) {
+        super(name, manufacturer, "AUDIO");
+        this.supportedAudioFormats = supportedAudioFormats;
+        this.SupportedPlaylistFormats = SupportedPlaylistFormats;
+    }
+
+
+    @Override
+    public void play() {
+        System.out.println("Playing");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopping");
+    }
+
+    @Override
+    public void previous() {
+        System.out.println("Previous");
+    }
+
+    @Override
+    public void next() {
+        System.out.println("Next");
+    }
+
+    public String toString() {
+        return super.toString() + "\nSupported Audio Formats: " + supportedAudioFormats + "\nSupported Playlist Formats: " + SupportedPlaylistFormats;
+    }
+
+}
---
+++ src/main/java/MultimediaControl.java
@@ -0,0 +1,11 @@
+public interface MultimediaControl {
+
+    public void play();
+
+    public void stop();
+
+    public void previous();
+
+    public void next();
+
+}
